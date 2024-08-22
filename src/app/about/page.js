a1ac6aa17ff293961f15ebaad244fe3caa914327
ShaYri12 <sharjeelb60@gmail.com>
2024-08-23 04:23:04 +0500

    header and footer work still remaining

---
+++ src/app/about/page.js
@@ -0,0 +1,15 @@
+import Header from "@/app/components/Header";
+import React from "react";
+import Footer from "../components/Footer";
+
+const About = () => {
+  return (
+    <div className="bg-[#192227]">
+      <Header />
+      <div className="text-white">About</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default About;
