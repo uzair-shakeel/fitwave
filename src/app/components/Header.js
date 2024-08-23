8f5d223775b6dbe0e924597f637b9e08e8fccc66
ShaYri12 <sharjeelb60@gmail.com>
2024-08-23 23:38:17 +0500

    fix header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,11 +3,26 @@ import { IoMdArrowRoundForward } from "react-icons/io";
 
 const Header = () => {
   return (
-    <header className="max-w-[1920px] mx-auto text-gray-600 bg-transparent px-[15px]">
-      <div
-        className="flex flex-wrap py-[0.556vw] flex-col md:flex-row items-center gap-[2.222vw]"
-        bis_skin_checked="1"
-      >
+    <header className="max-w-[1920px] mx-auto bg-transparent px-[15px]">
+      <div className="fixed top-[0.556vw] left-0 px-[15px] w-full flex gap-[6px] justify-center items-center h-[2.431vw] bg-orangish rounded-full text-center">
+        <p className="text-[1.042vw] text-transparent text-blackish font-[600] tracking-[-0.02em]">
+          We’ve officially launched! Learn more
+        </p>
+        <span className="flex justify-center items-center bg-[#192227] rounded-full w-[17px] h-[17px]">
+          <svg
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="Banner__Arrow-sc-1wnzso5-2 McOD"
+          >
+            <path
+              d="M4.86045 4.06812L11.7851 4.2145L11.9312 11.1395L10.0078 13.0629L9.8995 7.51404L5.42101 11.9925L4.0068 10.5783L8.48528 6.09982L2.93701 5.99155L4.86045 4.06812Z"
+              fill="#f9a600"
+            ></path>
+          </svg>
+        </span>
+      </div>
+      <div className="flex flex-wrap pt-[54px] py-[0.556vw] flex-col md:flex-row items-center gap-[2.222vw]">
         <Link
           href="/"
           className="flex title-font font-medium items-center text-white mb-4 md:mb-0 max-h-[18px] max-w-[80px]"
@@ -67,10 +82,10 @@ const Header = () => {
             href="/"
             className="flex items-center gap-[0.521vw] hover:text-gray-100"
           >
-            <IoMdArrowRoundForward size={16} /> Login
+            <IoMdArrowRoundForward size={16} /> Log in
           </Link>
         </nav>
-        <button className="inline-flex gap-[0.694vw] bg-[#f9a600] items-center py-[0.694vw] px-[1.042vw] text-[#192227] rounded-full mt-4 md:mt-0">
+        <button className="inline-flex gap-[0.694vw] bg-[#f9a600] items-center py-[7px] px-[15px] justify-center w-[11.806vw] text-[#192227] rounded-full mt-4 md:mt-0">
           <span className="text-[1.042vw] font-[600] tracking-[-0.02em]">
             Get Access{" "}
           </span>{" "}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orangish: "#f9a600",
+        blackish: "#192227",
+      },
+    },
   },
   plugins: [],
 };
