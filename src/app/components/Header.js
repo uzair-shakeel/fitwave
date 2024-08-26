2b1946a183115841727ccb7a43345989aac2b9d9
ShaYri12 <sharjeelb60@gmail.com>
2024-08-27 03:21:45 +0500

    header changes

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,7 +3,7 @@ import { IoMdArrowRoundForward } from "react-icons/io";
 
 const Header = () => {
   return (
-    <header className="max-w-[1920px] mx-auto bg-transparent px-[15px] bg-blackish">
+    <header className="max-w-[1920px] mx-auto bg-blackish px-[15px]">
       <div className="fixed top-[0.556vw] left-0 px-[15px] w-full z-[999]">
         <div className="px-[15px] w-full flex gap-[6px] justify-center items-center h-[2.431vw] bg-orangish rounded-full text-center">
           <p className="text-[1.042vw] text-blackish font-[600] tracking-[-0.02em]">
@@ -42,16 +42,16 @@ const Header = () => {
             href="/"
             className="flex items-center gap-[0.521vw] hover:text-gray-100"
           >
-            <IoMdArrowRoundForward size={16} /> Customers
+            <IoMdArrowRoundForward size={16} /> About
           </Link>
           <Link
             href="/about"
             className="flex items-center gap-[0.521vw] hover:text-gray-100"
           >
-            <IoMdArrowRoundForward size={16} /> Resources
+            <IoMdArrowRoundForward size={16} /> Contact
           </Link>
           <Link
-            href="/"
+            href="/contact"
             className="flex items-center gap-[0.521vw] hover:text-gray-100"
           >
             <IoMdArrowRoundForward size={16} /> Log in
