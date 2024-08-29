b329ee16104a720278488300c63b80046d8459b0
ShaYri12 <sharjeelb60@gmail.com>
2024-08-29 05:09:30 +0500

    ready one is responsive

--- src/app/components/home/ReadyforTakeoff.js
+++ src/app/components/home/ReadyforTakeoff.js
@@ -76,77 +76,113 @@ const ReadyforTakeoff = () => {
   return (
     <section className="bg-blue-300">
       <div className="min-h-screen bg-blackish text-white text-center rounded-[1.389vw] overflow-hidden">
-        <div className="mt-[1.389vw] flex gap-[1.042vw] px-[1.2vw]">
-          <div className="flex flex-col w-[31.319vw] h-[17.708vw] rounded-[1.389vw] p-[1.875vw] bg-orangish">
-            <span className="bg-blackish w-[4vw] h-full flex items-center justify-center rounded-full">
+        <div className="mt-[1.389vw] flex sm:flex-row flex-col gap-[1.042vw] px-[1.2vw]">
+          <div className="relative flex flex-col sm:w-[31.319vw] sm:min-h-[17.708vw] min-h-[28vw] sm:rounded-[1.389vw] rounded-[5.333vw] p-[1.875vw] sm:ps-[1.875vw] ps-[4.267vw] bg-orangish">
+            <span className="sm:flex hidden bg-blackish w-[4vw] h-full items-center justify-center rounded-full">
               <img
                 className="w-[2vw]"
                 src="/assets/arrow-right.svg"
                 alt="arrow-right"
               />
             </span>
-            <h2 className="text-left text-[3.125vw] font-[600] leading-[90%] tracking-[-0.04em] text-blackish w-[22.083vw] mt-[4.653vw] mb-[0.972vw]">
+            <h2 className="text-left sm:text-[3.125vw] text-[6.667vw] font-[600] sm:leading-[90%] leading-[100%] tracking-[-0.04em] text-blackish sm:w-[22.083vw] w-[45.333vw] sm:mt-[4.653vw] sm:mb-[0.972vw]">
               {" "}
               Supported By
             </h2>
-            <p className="text-left text-[1.528vw] font-[400] leading-[125%] tracking-[-0.02em] text-blackish ">
+            <p className="text-left sm:text-[1.528vw] text-[5.867vw] font-[400] leading-[125%] tracking-[-0.02em] text-blackish sm:w-auto w-[56vw] ">
               Investors and founders we admire
             </p>
+            <span className="absolute sm:hidden flex bg-blackish w-[6.6vw] h-[6.6vw] items-center justify-center rounded-full right-[3.5vw] bottom-[7.5vw]">
+              <span className="rotate-[90deg]">
+                <img
+                  className="w-[2.5vw]"
+                  src="/assets/arrow-right.svg"
+                  alt="arrow-right"
+                />
+              </span>
+            </span>
           </div>
-          <div className="w-[64.931vw] min-h-[17.708vw] rounded-[1.389vw] border border-[#fdfcfc]">
-            <div className="h-[5.833vw] border-b border-gray-600 ">
-              <Marquee {...settings1} direction="left" className="h-[5.833vw]">
-                {Data1.map((brand) => (
-                  <div>
-                    {
+          <div className="sm:w-[64.931vw] w-full min-h-[17.708vw] rounded-[1.389vw] border border-[#fdfcfc]">
+            <div className="sm:h-[5.833vw] h-[11.467vw] border-b border-gray-600 ">
+              <Marquee
+                {...settings1}
+                direction="left"
+                className="sm:h-[5.833vw] h-[11.467vw] my-auto"
+              >
+                <div className="flex ">
+                  {" "}
+                  {/* Adjust space-x-4 to control spacing */}
+                  {Data1.map((brand) => (
+                    <div key={brand}>
+                      {" "}
+                      {/* Add a key prop for better performance */}
                       <img
-                        className="h-[3.472vw] w-[10.764vw]"
+                        className="sm:h-[3.472vw] h-[6.667vw] sm:w-[10.764vw] w-[20.8vw] object-contain"
                         src={brand}
                         alt="logo"
                       />
-                    }
-                  </div>
-                ))}
+                    </div>
+                  ))}
+                </div>
               </Marquee>
             </div>
-            <div className="h-[5.833vw] border-b border-gray-600 ">
-              <Marquee {...settings2} direction="left" className="h-[5.833vw]">
-                {Data2.map((brand) => (
-                  <div>
-                    {
+            <div className="sm:h-[5.833vw] h-[11.467vw] border-b border-gray-600 ">
+              <Marquee
+                {...settings2}
+                direction="left"
+                className="sm:h-[5.833vw] h-[11.467vw] my-auto"
+              >
+                <div className="flex ">
+                  {" "}
+                  {/* Adjust space-x-4 to control spacing */}
+                  {Data2.map((brand) => (
+                    <div key={brand}>
+                      {" "}
+                      {/* Add a key prop for better performance */}
                       <img
-                        className="h-[3.472vw] w-[10.764vw] "
+                        className="sm:h-[3.472vw] h-[6.667vw] sm:w-[10.764vw] w-[20.8vw] object-contain"
                         src={brand}
                         alt="logo"
                       />
-                    }
-                  </div>
-                ))}
+                    </div>
+                  ))}
+                </div>
               </Marquee>
             </div>
-            <div className="h-[5.833vw]">
-              <Marquee {...settings3} direction="left" className="h-[5.833vw]">
-                {Data3.map((brand) => (
-                  <div>
-                    {
+            <div className="sm:h-[5.833vw] h-[11.467vw] border-b border-gray-600 ">
+              <Marquee
+                {...settings3}
+                direction="left"
+                className="sm:h-[5.833vw] h-[11.467vw] my-auto"
+              >
+                <div className="flex ">
+                  {" "}
+                  {/* Adjust space-x-4 to control spacing */}
+                  {Data3.map((brand) => (
+                    <div key={brand}>
+                      {" "}
+                      {/* Add a key prop for better performance */}
                       <img
-                        className="h-[3.472vw] w-[10.764vw]"
+                        className="sm:h-[3.472vw] h-[6.667vw] sm:w-[10.764vw] w-[20.8vw] object-contain"
                         src={brand}
                         alt="logo"
                       />
-                    }
-                  </div>
-                ))}
+                    </div>
+                  ))}
+                </div>
               </Marquee>
             </div>
           </div>
         </div>
         <div className="relative">
+          <div className="w-[18vw] h-[17vw] absolute ms-[2vw] top-[30px] z-[9] bg-gradient-to-t from-transparent to-blackish"></div>
+
           <svg
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 1330 1015"
             className="sc-06-SocialProof__Paths-sc-pkobnw-11 fwYvts absolute ps-[5vw] top-[30px]"
+            style={{ height: "-webkit-fill-available" }}
           >
             <g clipPath="url(#socialPaths_svg__clip0_2777_197741)">
               <path
@@ -180,16 +216,16 @@ const ReadyforTakeoff = () => {
               </clipPath>
             </defs>
           </svg>
-          <div className="pt-[26.042vw] pb-[8.333vw] ps-[33.264vw]">
+          <div className="sm:pt-[26.042vw] pt-[55.8vw] sm:pb-[8.333vw] pb-[6.667vw] sm:ps-[30.264vw] ps-[17.267vw]  flex flex-col sm:gap-0 gap-[4vw]">
             <div className="flex justify-center items-center w-fit bg-orangish rounded-full mb-[-10px]">
-              <span className="w-[74.9px] h-[74.9px] p-[2px] flex items-center justify-center">
-                <img src="/assets/plan.svg" alt="plan" />
+              <span className="sm:w-[74.9px] w-[11vw] sm:h-[74.9px] h-[11vw] p-[2px] flex items-center justify-center">
+                <img cl src="/assets/plan.svg" alt="plan" />
               </span>
             </div>
-            <h2 className="lg:text-[10.069vw] md:text-[12.207vw] text-[10.069vw] leading-[90%] tracking-[-0.04em] text-white font-[600] py-[2.083vw] w-[53.125vw] text-left">
-              Ready for Takeoff
+            <h2 className="sm:text-[10.069vw] text-[12vw] leading-[90%] tracking-[-0.04em] text-white font-[600] sm:py-[2.083vw] w-[53.125vw] text-left">
+              Ready for Takeoff?
             </h2>{" "}
-            <p className="text-left lg:text-[1.736vw] md:text-[2.441vw] text-[1.736vw] tracking-[-0.02em] leading-[125%] font-[400] text-orangish my-[3.083vw] w-[33vw]">
+            <p className="text-left sm:text-[1.736vw] text-[4vw] tracking-[-0.02em] leading-[125%] font-[400] text-orangish sm:my-[3.083vw] sm:w-[33vw] w-[53vw]">
               Runway is ready to help you and your team get off the ground.
             </p>
           </div>
